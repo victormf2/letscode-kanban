b28d4b579553167535b564c998e157f9ad960f27
Victor Machado <victor_mf2@hotmail.com>
2021-06-27 02:35:50 -0300

    [frontend] login form validation

--- frontend/src/app/login/login.component.ts
+++ frontend/src/app/login/login.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormControl, FormGroup } from '@angular/forms';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 import { LoginService } from './login.service';
 
@@ -17,8 +17,12 @@ export class LoginComponent implements OnInit {
     readonly router: Router,
   ) {
     this.loginForm = new FormGroup({
-      username: new FormControl(''),
-      password: new FormControl(''),
+      username: new FormControl('', [
+        Validators.required
+      ]),
+      password: new FormControl('', [
+        Validators.required
+      ]),
     })
   }
 
